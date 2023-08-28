e55ba02398d4fa13f583b2e281cb056a8449e2e2
NgocMinhThuNguyen <94945964+NgocMinhThuNguyen@users.noreply.github.com>
2023-08-28 21:53:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const ratings = document.querySelectorAll(".rating button");
-const submitBtn = document.querySelector('.call-to-action');
+const submitBtn = document.querySelector('.submit');
 const thankYou = document.querySelector('.thankyou-state');
 let userRating = '';
 
@@ -7,12 +7,18 @@ let userRating = '';
 for (let i=0; i < ratings.length; i++) {
   ratings[i].addEventListener('click', event => {
     event.preventDefault();
-    
+
     userRating = ratings[i].innerHTML;
   })
 }
 
 submitBtn.addEventListener('click', () => {
-  let selectedRating = document.querySelector("span").innerText = userRating;
+  let selectedRating = document.querySelector("span");
+
+  if (userRating === '') {
+    selectedRating.innerText = '0';
+  } else {
+    selectedRating.innerText = userRating;
+  }
   thankYou.classList.add('after');
 });
